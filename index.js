21ac411121b1ed7300c5c4fabd48e47edb180e2c
Obala <oballasteve4@gmail.com>
2024-03-26 08:22:16 +0300

    adds arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+console.log(divide()); 
+
+const square =num1=>num1*num1;
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
